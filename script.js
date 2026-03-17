9b33782e7a98d8c1e9858ac2d10bdcc725f85548
LPScoder <arthur_l_garcia@estudante.sesisenai.org.br>
2026-03-17 20:59:39 -0300

    Fix - Function Calcular.

--- script.js
+++ script.js
@@ -8,4 +8,16 @@ function clearDisplay() {
 
 function calcular() {
     let conta = document.getElementById("display").value;
-}
+    
+    try {
+            if (conta.includes("/0")) {
+                document.getElementById("display").value = "Error";
+                return;
+            }
+
+            let resultado = eval(conta);
+            document.getElementById("display").value = resultado;
+        } catch {
+            document.getElementById("display").value = "Error";
+        }
+    }
